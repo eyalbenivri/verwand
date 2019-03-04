6a3d484abd855eadd8c9368c576c9158d95722d8
Eyal Ben Ivri <eyal.ben-ivri@researchgate.net>
2019-03-04 17:38:53 +0100

    Project setup

---
+++ verwand-web/src/main/java/net/bobis/verwand/VerwandWebApplication.java
@@ -0,0 +1,13 @@
+package net.bobis.verwand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerwandWebApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(VerwandWebApplication.class, args);
+    }
+    
+}
